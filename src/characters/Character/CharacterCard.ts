ba1db12c6e5e319ab34ea64a84200019be29c37d
Víctor Andújar <ammavaru@Victors-MacBook-Air.local>
2023-01-23 22:39:02 +0100

    Add interface at character card structure

---
+++ src/characters/Character/CharacterCard.ts
@@ -0,0 +1,14 @@
+interface CharacterCardStructure {
+  element: Element;
+}
+
+class CharacterCard {
+  element: Element;
+
+  constructor(parentElement: Element) {
+    this.element = document.createElement("article");
+    parentElement.appendChild(this.element);
+  }
+}
+
+export default CharacterCard;
