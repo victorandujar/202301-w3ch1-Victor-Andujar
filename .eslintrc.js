4d8d248855784768e5583f73979abc795f016f75
Víctor Andújar <ammavaru@Victors-MacBook-Air.local>
2023-01-24 17:38:23 +0100

    Add interface eslint rule and change type for interface at types.ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,12 @@ module.exports = {
     {
       extends: ["xo-typescript", "prettier"],
       files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": [
+          "error",
+          "interface",
+        ],
+      },
     },
   ],
   parserOptions: {
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Advisor } from "./characters/Advisor/Advisor";
 import { Fighter } from "./characters/Fighter/Fighter";
 import { King } from "./characters/King/King";
 import { Squire } from "./characters/Squire/Squire";
+import CharacterCard from "./characters/Character/CharacterCard";
 
 import type {
   AdvisorStructre,
